5054d19fa6207ddc40e94c99f9f7701e91a6410c
Loganathan G P <logusivam26@gmail.com>
2024-08-22 09:54:44 +0000

    Aug 22, 2024, 3:24 PM

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 document.querySelector('.hideshow').addEventListener('click', function() 
 {
     const passwordInput = document.querySelector('.pas');
-    const eyeIcon = document.querySelector()
-})
+    const eyeIcon = document.querySelector('.hideshow');
+
+    /* checks the password for the icon change */
+    if(passwordInput.type === 'password')
+    {
+        passwordInput.type = 'text';
+        eyeIcon.setAttribute('name', 'eye-outline');  /* changes to open eye */
+    }
+
+    else
+    {
+        passwordInput.type = 'password';
+        eyeIcon.setAttribute('name', 'eye-off-outline');  /* changes to close eye */
+    }
+});
